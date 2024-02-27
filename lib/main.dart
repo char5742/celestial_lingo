17821c7866f59153bc8d08cc5eca2a67aa6ff332
char5742 <fujino@stellarcode.jp>
2024-02-27 18:19:30 +0900

    update: setUrlStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
@@ -6,7 +7,7 @@ import 'providers/chat_service_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  setUrlStrategy(PathUrlStrategy());
   final container = ProviderContainer();
   await container.read(chatServiceProvider).initialize();
   runApp(
